806c73ae193e5e5ed73e4cf32b624a1b3af67715
VijayaprakashVp <prakashvijay905@gmail.com>
2022-06-20 15:09:40 +0530

    Update Users.jsx

--- Users-App/src/Components/Users.jsx
+++ Users-App/src/Components/Users.jsx
@@ -3,6 +3,11 @@ import { useQuery, useMutation } from "react-query";
 import axios from "axios";
 
 export const Users = () => {
+  const [showform, setShowform] = useState(false);
+  const [singleUser, setSingleUser] = useState([]);
+  const [name, setName] = useState("");
+  const [imageurl, setImageurl] = useState("");
+
   const { isLoading, error, data } = useQuery("userData", () =>
     axios(`https://62b008c7e460b79df03b7410.mockapi.io/users`),
   );
@@ -11,14 +16,9 @@ export const Users = () => {
   if (isLoading) return <h1>Loading...</h1>;
   if (error) return <h1>error : {error.message}</h1>;
 
-  const [showform, setShowform] = useState(false);
-  const [singleUser, setSingleUser] = useState([]);
-  const [name, setName] = useState("");
-  const [imageurl, setImageurl] = useState("");
-
   const handleEditSubmit = () => {
-    console.log("name:", name);
-    console.log("imageurl:", imageurl);
+    // console.log("name:", name);
+    // console.log("imageurl:", imageurl);
 
     fetch(
       `https://62b008c7e460b79df03b7410.mockapi.io/users/${singleUser.id}`,
@@ -116,50 +116,3 @@ export const Users = () => {
     </div>
   );
 };
-
-////////////////////////////////////////////////////////////
-
-// const [data, setData] = useState([]);
-//   const [edit, setEdit] = useState({});
-//   var refresh = 0;
-
-//   useEffect(() => {
-//     fetch(`https://62b008c7e460b79df03b7410.mockapi.io/users`)
-//       .then((res) => res.json())
-//       .then((res) => setData(res))
-//       .catch((err) => console.log(err));
-//   }, [refresh]);
-
-//   const handleChange = (e) => {
-//     setEdit({ ...edit, [e.target.name]: e.target.value });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Users List</h1>
-//       <table style={{ margin: "auto" }}>
-//         <tr>
-//           <th>S.No</th>
-//           <th>Name</th>
-//           <th>Avatar</th>
-//         </tr>
-//         {data.map((e, i) => (
-//           <tr key={e.id}>
-//             <td>{i + 1 + "."}</td>
-//             <td>{e.name}</td>
-//             <img src={e.avatar} alt="" style={{ width: "50px" }} />
-//             <td>
-//               <button
-//                 style={{ marginLeft: "50px", cursor: "pointer" }}
-//                 onClick={(id) => handleEdit(e.id)}
-//               >
-//                 Edit
-//               </button>
-//             </td>
-//           </tr>
-//         ))}
-//       </table>
-
-//
-//     </div>
-//   );
