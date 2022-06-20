4833d5405a4293fb7bb380bb8bdb9ab48f7db155
VijayaprakashVp <prakashvijay905@gmail.com>
2022-06-20 12:42:31 +0530

    Can edit the particular User

--- Users-App/src/Components/Users.jsx
+++ Users-App/src/Components/Users.jsx
@@ -1,5 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Users = () => {
-  return <div>Users</div>;
+  const [data, setData] = useState([]);
+  const [showform, setShowform] = useState(false);
+  const [singleUser, setSingleUser] = useState([]);
+  const [edit, setEdit] = useState({});
+  var refresh = 0;
+
+  useEffect(() => {
+    fetch(`https://62b008c7e460b79df03b7410.mockapi.io/users`)
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.log(err));
+  }, [refresh]);
+
+  const handleEdit = (id) => {
+    fetch(`https://62b008c7e460b79df03b7410.mockapi.io/users/${id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setSingleUser(res);
+        setShowform(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleChange = (e) => {
+    setEdit({ ...edit, [e.target.name]: e.target.value });
+  };
+
+  const handleEditSubmit = () => {
+    fetch(
+      `https://62b008c7e460b79df03b7410.mockapi.io/users/${singleUser.id}`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+
+    fetch(`https://62b008c7e460b79df03b7410.mockapi.io/users/`, {
+      method: "POST",
+      body: JSON.stringify(edit),
+      headers: { "Content-Type": "application/json" },
+    });
+    refresh++;
+  };
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <table style={{ margin: "auto" }}>
+        <tr>
+          <th>S.No</th>
+          <th>Name</th>
+          <th>Avatar</th>
+        </tr>
+        {data.map((e, i) => (
+          <tr key={e.id}>
+            <td>{i + 1 + "."}</td>
+            <td>{e.name}</td>
+            <img src={e.avatar} alt="" style={{ width: "50px" }} />
+            <td>
+              <button
+                style={{ marginLeft: "50px", cursor: "pointer" }}
+                onClick={(id) => handleEdit(e.id)}
+              >
+                Edit
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+
+      {showform ? (
+        <div style={{ marginBottom: "100px" }}>
+          <h4>
+            {singleUser.name}'s Data , can{" "}
+            <span style={{ color: "red" }}>Edit</span> here
+          </h4>
+          <form action="">
+            Name :{" "}
+            <input
+              type="text"
+              placeholder={singleUser.name}
+              name="name"
+              onChange={(e) => handleChange(e)}
+              value={edit.name}
+            />
+            <br /> <br />
+            Avatar :{" "}
+            <input
+              type="text"
+              placeholder="place enter avatar url"
+              name="avatar"
+              onChange={(e) => handleChange(e)}
+              value={edit.avatar}
+            />
+            <br />
+          </form>
+          <br />
+          <button onClick={handleEditSubmit}>Submit</button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
